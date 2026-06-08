67739c350d9396b9904b1af2cba88651fa3b46b7
Toto-glitch <sergeevanton03072007@gmail.com>
2026-06-08 15:37:52 +0900

    Add lifespan for app

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 from fastapi import FastAPI
-from core import settings
 import uvicorn
 
+from core import settings, close_connections
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(fastapi_app: FastAPI) -> AsyncGenerator[None, None]:
+    # startup
+    print("starting app...")
+    yield
+    # shoutdown
+    print("closing app...")
+    await close_connections()
+
+
+app = FastAPI(lifespan=lifespan)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host=settings.uc.host, port=settings.uc.port, reload=settings.uc.reload)
