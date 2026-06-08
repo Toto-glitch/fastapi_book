c766af3bd02f73414fca24b86ed55dab08f2d92d
Toto-glitch <sergeevanton03072007@gmail.com>
2026-06-08 15:49:55 +0900

    Add naming constraints

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,8 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.sql.schema import MetaData
+
 from .config import settings
 
 engine = create_async_engine(
@@ -13,7 +15,15 @@ SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
-    pass
+    convention = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
+
+    metadata = MetaData(naming_convention=convention)
 
 
 async def close_connections() -> None:
