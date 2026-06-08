c655cb5f12cf1b510f919496d8f4ec4cddc8a54d
Toto-glitch <sergeevanton03072007@gmail.com>
2026-06-08 14:01:47 +0900

    Create config for uvicorn

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class UvicornConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 8000
+    reload: bool = True
+
+
+class Settings(BaseSettings):
+    uc: UvicornConfig = UvicornConfig()
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_prefix="APP_",
+        env_nested_delimiter="_"
+    )
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from core import settings
 import uvicorn
+
 
 app = FastAPI()
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host=settings.uc.host, port=settings.uc.port, reload=settings.uc.reload)
