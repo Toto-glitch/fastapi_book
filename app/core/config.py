7f322a0d8db97c34cb128ede1bc47e63ca9231ed
Toto-glitch <sergeevanton03072007@gmail.com>
2026-06-08 15:35:02 +0900

    Create database config

--- app/core/config.py
+++ app/core/config.py
@@ -8,8 +8,24 @@ class UvicornConfig(BaseModel):
     reload: bool
 
 
+class DatabaseConfig(BaseModel):
+    host: str
+    port: int
+    user: str
+    password: str
+    name: str
+    echo: bool
+    pool_size: int
+    max_overflow: int
+
+    @property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+
+
 class Settings(BaseSettings):
     uc: UvicornConfig
+    db: DatabaseConfig
 
     model_config = SettingsConfigDict(
         env_file=(".env.example", ".env"),
