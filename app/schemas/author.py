c291f23f5af33363bb4c547e95a52e660d4ab235
Toto-glitch <sergeevanton03072007@gmail.com>
2026-06-10 17:41:13 +0900

    Add from attributes to AuthorResponse schema

--- app/schemas/author.py
+++ app/schemas/author.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class AuthorBase(BaseModel):
@@ -14,3 +14,5 @@ class AuthorCreate(AuthorBase):
 
 class AuthorResponse(AuthorBase):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
