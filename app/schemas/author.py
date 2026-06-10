921afd818f4067df6e8c80acdacf5176c7c2c722
Toto-glitch <sergeevanton03072007@gmail.com>
2026-06-10 17:34:46 +0900

    Create author schemas

---
+++ app/schemas/author.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+
+
+class AuthorBase(BaseModel):
+    first_name: str = Field(..., max_length=255, min_length=5)
+    last_name: str = Field(..., max_length=255, min_length=5)
+    father_name: str | None = Field(None, max_length=255, min_length=5)
+    birth_year: int | None = Field(None, ge=1, le=9999)
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class AuthorResponse(AuthorBase):
+    id: int
