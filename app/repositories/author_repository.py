bf4564fb25cea6fc1e14d2f7c534d7019e459168
Toto-glitch <sergeevanton03072007@gmail.com>
2026-07-13 13:41:13 +0900

    Add get_books() method in author repository class

--- app/repositories/author_repository.py
+++ app/repositories/author_repository.py
@@ -1,6 +1,13 @@
+from sqlalchemy import Sequence, select
+
 from .base import BaseRepository
-from models import Author
+from models import Author, Book
 
 
 class AuthorRepository(BaseRepository):
     model = Author
+
+    async def get_books(self, author_id: int, offset: int = 0, limit: int = 20) -> Sequence[Book]:
+        query = select(Book).filter_by(author_id=author_id).offset(offset).limit(limit)
+        query_result = await self.session.execute(query)
+        return query_result.scalars().all()
