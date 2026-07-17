c62c1b296828652bd2d9a16736e30b24942f07a1
Toto-glitch <sergeevanton03072007@gmail.com>
2026-07-17 13:22:52 +0900

    Include genres_router to app

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
+
 from .authors import authors_router
 from .books import books_router
+from .genres import genres_router
 
 main_router = APIRouter(prefix="/api")
 main_router.include_router(authors_router)
 main_router.include_router(books_router)
+main_router.include_router(genres_router)
