1180b5b9a4390862f6ac57c57a0102f3482e075c
Toto-glitch <sergeevanton03072007@gmail.com>
2026-07-19 14:14:42 +0900

    Update get_books and count_books in book repository

--- app/repositories/author_repository.py
+++ app/repositories/author_repository.py
@@ -11,11 +11,21 @@ class AuthorRepository(BaseRepository[Author]):
     async def get_books(
         self, author_id: int, offset: int = 0, limit: int = 20
     ) -> Sequence[Book]:
-        query = select(Book).filter_by(author_id=author_id).offset(offset).limit(limit)
+        query = (
+            select(Book)
+            .join(Book.author)
+            .filter(Author.id == author_id)
+            .offset(offset)
+            .limit(limit)
+        )
         query_result = await self.session.execute(query)
         return query_result.scalars().all()
 
     async def count_books(self, author_id: int) -> int:
-        query = select(func.count()).select_from(Book).filter_by(author_id=author_id)
+        query = (
+            select(func.count(Book.id))
+            .join(Book.author)
+            .filter(Author.id == author_id)
+        )
         query_result = await self.session.execute(query)
         return query_result.scalar_one()
