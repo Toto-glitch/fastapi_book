b2a95548f9545735602abce95b8d14e7b65821ff
Toto-glitch <sergeevanton03072007@gmail.com>
2026-07-19 14:13:59 +0900

    Update get_genres and count_genres in book repository

--- app/repositories/book_repository.py
+++ app/repositories/book_repository.py
@@ -1,6 +1,29 @@
+from sqlalchemy import select, func
+from typing import Sequence
+
 from .base import BaseRepository
-from models import Book
+from models import Book, Genre
 
 
 class BookRepository(BaseRepository[Book]):
     model = Book
+
+    async def get_genres(self, book_id: int, offset: int = 0, limit: int = 20) -> Sequence[Genre]:
+        query = (
+            select(Genre)
+            .join(Genre.books)
+            .filter(Book.id == book_id)
+            .offset(offset)
+            .limit(limit)
+        )
+        query_result = await self.session.execute(query)
+        return query_result.scalars().all()
+
+    async def count_genres(self, book_id: int) -> int:
+        query = (
+            select(func.count(Genre.id))
+            .join(Genre.books)
+            .filter(Book.id == book_id)
+        )
+        query_result = await self.session.execute(query)
+        return query_result.scalar_one()
